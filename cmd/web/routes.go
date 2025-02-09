c96f8b6d48b9bc068439e801a3e53c6355880d73
Sanjay Ojha <ojha.phy21@gmail.com>
2025-02-09 13:19:52 +0530

    file embedding and binary

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -4,13 +4,15 @@ import (
 	"net/http"
 
 	"github.com/justinas/alice"
+	"snippetbox.sanjayojha.in/ui"
 )
 
 func (app *application) routes() http.Handler {
 
 	mux := http.NewServeMux()
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
+	// fileServer := http.FileServer(http.Dir("./ui/static/"))
+	// mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
+	mux.Handle("GET /static/", http.FileServerFS(ui.Files))
 
 	// dynamic middleware
 	dynamicMiddleware := alice.New(app.sessionManager.LoadAndSave, noSurf, app.authenticate)
--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"html/template"
+	"io/fs"
 	"path/filepath"
 	"time"
 
 	"snippetbox.sanjayojha.in/internal/models"
+	"snippetbox.sanjayojha.in/ui"
 )
 
 type templateData struct {
@@ -29,7 +31,8 @@ var functions = template.FuncMap{
 func newTemplateCache() (map[string]*template.Template, error) {
 	cache := map[string]*template.Template{}
 
-	pages, err := filepath.Glob("./ui/html/pages/*.tmpl")
+	//pages, err := filepath.Glob("./ui/html/pages/*.tmpl")
+	pages, err := fs.Glob(ui.Files, "html/pages/*.tmpl")
 
 	if err != nil {
 		return nil, err
@@ -38,21 +41,28 @@ func newTemplateCache() (map[string]*template.Template, error) {
 	for _, page := range pages {
 		name := filepath.Base(page)
 
+		patterns := []string{
+			"html/base.tmpl",
+			"html/partials/*.tmpl",
+			page,
+		}
+
 		//ts, err := template.ParseFiles("./ui/html/base.tmpl")
-		ts, err := template.New(name).Funcs(functions).ParseFiles("./ui/html/base.tmpl")
+		//ts, err := template.New(name).Funcs(functions).ParseFiles("./ui/html/base.tmpl")
+		ts, err := template.New(name).Funcs(functions).ParseFS(ui.Files, patterns...)
 		if err != nil {
 			return nil, err
 		}
 
-		ts, err = ts.ParseGlob("./ui/html/partials/*.tmpl")
-		if err != nil {
-			return nil, err
-		}
+		// ts, err = ts.ParseGlob("./ui/html/partials/*.tmpl")
+		// if err != nil {
+		// 	return nil, err
+		// }
 
-		ts, err = ts.ParseFiles(page)
-		if err != nil {
-			return nil, err
-		}
+		// ts, err = ts.ParseFiles(page)
+		// if err != nil {
+		// 	return nil, err
+		// }
 		cache[name] = ts
 	}
 	// Return the map.
